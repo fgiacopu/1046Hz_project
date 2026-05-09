b25c46b941284f12ff0f4c6eb6f768b37ecaa6e9
fgiacopu <196767608+fgiacopu@users.noreply.github.com>
2026-05-09 18:43:33 +0200

    Added .bat scripts to simplify process startup

---
+++ python/start_python.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ==============================
+echo Starting Gesture Control Python
+echo ==============================
+
+cd /d "C:\Users\Federico\Documents\polimi\corsi\cmls\1046Hz_project\python"
+
+"C:\Users\Federico\miniconda3\envs\cmls\python.exe" main.py
+
+echo.
+echo ==============================
+echo Python process ended
+echo ==============================
+pause
---
+++ supercollider/start_supercollider.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ===================================
+echo Starting SuperCollider Audio Engine
+echo ===================================
+
+cd /d "C:\Users\Federico\Documents\polimi\corsi\cmls\1046Hz_project\supercollider"
+
+"C:\Program Files\SuperCollider-3.14.1\sclang.exe" audio_engine_sclang.scd
+
+echo.
+echo ==============================
+echo Process ended
+echo ==============================
+pause
